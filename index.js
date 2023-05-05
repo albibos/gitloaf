0fff48b52dc206ac2dcda83ca600bd9c82ebd40f
albibos <99928204+albibos@users.noreply.github.com>
2023-05-05 00:21:12 +0000

    forgot to change version text

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 app.use(compression());
 
 app.use(express.static(path.join(__dirname, 'public')));
