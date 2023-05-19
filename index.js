b877b61c230f2c07657fb73dac10a4beffb5c73e
LEGALISE PIRACY <74979271+Rahib777-7@users.noreply.github.com>
2023-05-19 16:44:07 -0400

    add githubraw cdn

--- index.js
+++ index.js
@@ -544,6 +544,47 @@ app.get('/staticallycdn/:user/:repo/:branch/*', async (req, res) => {
     console.error(e);
   }
 });
+app.get('/githubrawcdn/:user/:repo/:branch/*', async (req, res) => {
+  const { user, repo, branch } = req.params;
+  const filePath = req.params[0];
+
+try {
+    const githubrawUrl = `https://cdn.githubraw.com/${user}/${repo}/${branch}/${filePath}`;
+    const cacheKey = `${user}-${repo}-${branch}-${filePath}`;
+    const cachedFile = fileCache.get(cacheKey);
+
+    if (cachedFile) {
+      res.setHeader('Cache-Control', 'public, max-age=600');
+      res.writeHead(cachedFile.status, { 'Content-Type': cachedFile.contentType.split(';')[0] });
+      res.end(cachedFile.content);
+    } else {
+      const response = await fetch(rawgitUrl);
+
+      if (response.status === 404) {
+        return res.sendStatus(404);
+      }
+
+      const contentType = response.headers.get('content-type');
+      if (contentType && contentType.startsWith('image/')) {
+        res.setHeader('Content-Type', contentType);
+        response.body.pipe(res);
+      } else {
+        const file = await fetchFile(githubrawUrl);
+        fileCache.set(cacheKey, file);
+        res.setHeader('Cache-Control', 'public, max-age=300');
+        res.writeHead(file.status, { 'Content-Type': file.contentType.split(';')[0] });
+
+        res.locals.fileContent = file.content;
+        await removeLeadingSlashFromAttributes(req, res, () => {
+          res.end(res.locals.fileContent);
+        });
+      }
+    }
+  } catch (e) {
+    res.sendStatus(404);
+    console.error(e);
+  }
+});
 
 const startLoadBalancer = require('./loadBalancer');
 startLoadBalancer();
