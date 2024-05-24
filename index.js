6b1fde9277caef7fa1f2dfb07dab0e46a80e73ac
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2024-05-23 20:58:35 -0700

    Migrate CommonJS to ES Module

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const path = require('path');
-const NodeCache = require('node-cache');
-const compression = require('compression');
-const fs = require('fs');
+import express from "express";
+import fetch from "node-fetch";
+import path from "path";
+import NodeCache from "node-cache";
+import compression from "compression";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import fs from "fs";
 const app = express();
 const port = process.env.PORT || 3000;
-const mime = JSON.parse(fs.readFileSync('./mime.json', 'utf8'));
+const mime = JSON.parse(fs.readFileSync("./mime.json", "utf8"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
 
   next();
 });
 
 app.use(compression());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const fileCache = new NodeCache({ stdTTL: 600, checkperiod: 600 });
 
 function rewriteURLAndRedirect(req, res, next) {
-  const user = '3kh0';
-  const repo = '3kh0-Assets';
-  const branch = 'main';
+  const user = "3kh0";
+  const repo = "3kh0-Assets";
+  const branch = "main";
 
-  if (req.path.startsWith('/js/') || req.path.startsWith('/css/') || req.path.startsWith('/json/')) {
+  if (
+    req.path.startsWith("/js/") ||
+    req.path.startsWith("/css/") ||
+    req.path.startsWith("/json/")
+  ) {
     const cdnPath = `/cdn/${user}/${repo}/${branch}${req.path}`;
     return res.redirect(cdnPath);
   }
@@ -35,15 +49,18 @@ function rewriteURLAndRedirect(req, res, next) {
   next();
 }
 
-
 async function fetchFile(url) {
   const response = await fetch(url);
   const content = await response.text();
 
-  let contentType = response.headers.get('content-type');
-  if (!contentType || contentType === 'text/plain' || contentType.includes('charset=utf-8')) {
+  let contentType = response.headers.get("content-type");
+  if (
+    !contentType ||
+    contentType === "text/plain" ||
+    contentType.includes("charset=utf-8")
+  ) {
     const ext = path.extname(url).slice(1);
-    contentType = mime[ext] || 'text/plain';
+    contentType = mime[ext] || "text/plain";
   }
 
   return {
@@ -58,21 +75,21 @@ async function fetchFile(url) {
 app.use(rewriteURLAndRedirect);
 
 async function removeLeadingSlashFromAttributes(req, res, next) {
-  if (!req.path.endsWith('.html')) {
+  if (!req.path.endsWith(".html")) {
     return next();
   }
 
   const originalContent = res.locals.fileContent;
   const modifiedContent = originalContent.replace(
     /(<(?:script|link|img|source)[^>]*(?:src|href|srcset)=["']?)\//g,
-    '$1'
+    "$1"
   );
 
   res.locals.fileContent = modifiedContent;
   next();
 }
 
-app.get('/cdn/:user/:repo/:branch/*', async (req, res) => {
+app.get("/cdn/:user/:repo/:branch/*", async (req, res) => {
   const { user, repo, branch } = req.params;
   const filePath = req.params[0];
 
@@ -82,8 +99,10 @@ app.get('/cdn/:user/:repo/:branch/*', async (req, res) => {
     const cachedFile = fileCache.get(cacheKey);
 
     if (cachedFile) {
-      res.setHeader('Cache-Control', 'public, max-age=600');
-      res.writeHead(cachedFile.status, { 'Content-Type': cachedFile.contentType.split(';')[0] });
+      res.setHeader("Cache-Control", "public, max-age=600");
+      res.writeHead(cachedFile.status, {
+        "Content-Type": cachedFile.contentType.split(";")[0],
+      });
       res.end(cachedFile.content);
     } else {
       const response = await fetch(githubUrl);
@@ -92,15 +111,17 @@ app.get('/cdn/:user/:repo/:branch/*', async (req, res) => {
         return res.sendStatus(404);
       }
 
-      const contentType = response.headers.get('content-type');
-      if (contentType && contentType.startsWith('image/')) {
-        res.setHeader('Content-Type', contentType);
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.startsWith("image/")) {
+        res.setHeader("Content-Type", contentType);
         response.body.pipe(res);
       } else {
         const file = await fetchFile(githubUrl);
         fileCache.set(cacheKey, file);
-        res.setHeader('Cache-Control', 'public, max-age=300');
-        res.writeHead(file.status, { 'Content-Type': file.contentType.split(';')[0] });
+        res.setHeader("Cache-Control", "public, max-age=300");
+        res.writeHead(file.status, {
+          "Content-Type": file.contentType.split(";")[0],
+        });
 
         res.locals.fileContent = file.content;
         await removeLeadingSlashFromAttributes(req, res, () => {
@@ -114,8 +135,7 @@ app.get('/cdn/:user/:repo/:branch/*', async (req, res) => {
   }
 });
 
-
-app.get('/jsdcdn/:user/:repo/:branch/*', async (req, res) => {
+app.get("/jsdcdn/:user/:repo/:branch/*", async (req, res) => {
   const { user, repo, branch } = req.params;
   const filePath = req.params[0];
 
@@ -127,15 +147,17 @@ app.get('/jsdcdn/:user/:repo/:branch/*', async (req, res) => {
       return res.sendStatus(404);
     }
 
-    res.setHeader('Cache-Control', 'public, max-age=600');
+    res.setHeader("Cache-Control", "public, max-age=600");
 
-    const contentType = response.headers.get('content-type');
-    if (contentType && contentType.startsWith('image/')) {
-      res.setHeader('Content-Type', contentType);
+    const contentType = response.headers.get("content-type");
+    if (contentType && contentType.startsWith("image/")) {
+      res.setHeader("Content-Type", contentType);
       response.body.pipe(res);
     } else {
       const file = await fetchFile(cloudflareCdnUrl);
-      res.writeHead(file.status, { 'Content-Type': file.contentType.split(';')[0] });
+      res.writeHead(file.status, {
+        "Content-Type": file.contentType.split(";")[0],
+      });
       res.end(file.content);
     }
   } catch (e) {
@@ -144,7 +166,7 @@ app.get('/jsdcdn/:user/:repo/:branch/*', async (req, res) => {
   }
 });
 
-app.get('/rgcdn/:user/:repo/:branch/*', async (req, res) => {
+app.get("/rgcdn/:user/:repo/:branch/*", async (req, res) => {
   const { user, repo, branch } = req.params;
   const filePath = req.params[0];
 
@@ -154,8 +176,10 @@ app.get('/rgcdn/:user/:repo/:branch/*', async (req, res) => {
     const cachedFile = fileCache.get(cacheKey);
 
     if (cachedFile) {
-      res.setHeader('Cache-Control', 'public, max-age=600');
-      res.writeHead(cachedFile.status, { 'Content-Type': cachedFile.contentType.split(';')[0] });
+      res.setHeader("Cache-Control", "public, max-age=600");
+      res.writeHead(cachedFile.status, {
+        "Content-Type": cachedFile.contentType.split(";")[0],
+      });
       res.end(cachedFile.content);
     } else {
       const response = await fetch(githackUrl);
@@ -164,15 +188,17 @@ app.get('/rgcdn/:user/:repo/:branch/*', async (req, res) => {
         return res.sendStatus(404);
       }
 
-      const contentType = response.headers.get('content-type');
-      if (contentType && contentType.startsWith('image/')) {
-        res.setHeader('Content-Type', contentType);
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.startsWith("image/")) {
+        res.setHeader("Content-Type", contentType);
         response.body.pipe(res);
       } else {
         const file = await fetchFile(githackUrl);
         fileCache.set(cacheKey, file);
-        res.setHeader('Cache-Control', 'public, max-age=300');
-        res.writeHead(file.status, { 'Content-Type': file.contentType.split(';')[0] });
+        res.setHeader("Cache-Control", "public, max-age=300");
+        res.writeHead(file.status, {
+          "Content-Type": file.contentType.split(";")[0],
+        });
 
         res.locals.fileContent = file.content;
         await removeLeadingSlashFromAttributes(req, res, () => {
@@ -186,19 +212,20 @@ app.get('/rgcdn/:user/:repo/:branch/*', async (req, res) => {
   }
 });
 
-
- app.get('/rawgcdn/:user/:repo/:branch/*', async (req, res) => {
+app.get("/rawgcdn/:user/:repo/:branch/*", async (req, res) => {
   const { user, repo, branch } = req.params;
   const filePath = req.params[0];
 
-try {
+  try {
     const rawgitUrl = `https://raw.rawgit.net/${user}/${repo}/${branch}/${filePath}`;
     const cacheKey = `${user}-${repo}-${branch}-${filePath}`;
     const cachedFile = fileCache.get(cacheKey);
 
     if (cachedFile) {
-      res.setHeader('Cache-Control', 'public, max-age=600');
-      res.writeHead(cachedFile.status, { 'Content-Type': cachedFile.contentType.split(';')[0] });
+      res.setHeader("Cache-Control", "public, max-age=600");
+      res.writeHead(cachedFile.status, {
+        "Content-Type": cachedFile.contentType.split(";")[0],
+      });
       res.end(cachedFile.content);
     } else {
       const response = await fetch(rawgitUrl);
@@ -207,15 +234,17 @@ try {
         return res.sendStatus(404);
       }
 
-      const contentType = response.headers.get('content-type');
-      if (contentType && contentType.startsWith('image/')) {
-        res.setHeader('Content-Type', contentType);
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.startsWith("image/")) {
+        res.setHeader("Content-Type", contentType);
         response.body.pipe(res);
       } else {
         const file = await fetchFile(rawgitUrl);
         fileCache.set(cacheKey, file);
-        res.setHeader('Cache-Control', 'public, max-age=300');
-        res.writeHead(file.status, { 'Content-Type': file.contentType.split(';')[0] });
+        res.setHeader("Cache-Control", "public, max-age=300");
+        res.writeHead(file.status, {
+          "Content-Type": file.contentType.split(";")[0],
+        });
 
         res.locals.fileContent = file.content;
         await removeLeadingSlashFromAttributes(req, res, () => {
@@ -229,7 +258,7 @@ try {
   }
 });
 
-app.get('/gitcfcdn/:user/:repo/:branch/*', async (req, res) => {
+app.get("/gitcfcdn/:user/:repo/:branch/*", async (req, res) => {
   const { user, repo, branch } = req.params;
   const filePath = req.params[0];
 
@@ -239,8 +268,10 @@ app.get('/gitcfcdn/:user/:repo/:branch/*', async (req, res) => {
     const cachedFile = fileCache.get(cacheKey);
 
     if (cachedFile) {
-      res.setHeader('Cache-Control', 'public, max-age=600');
-      res.writeHead(cachedFile.status, { 'Content-Type': cachedFile.contentType.split(';')[0] });
+      res.setHeader("Cache-Control", "public, max-age=600");
+      res.writeHead(cachedFile.status, {
+        "Content-Type": cachedFile.contentType.split(";")[0],
+      });
       res.end(cachedFile.content);
     } else {
       const response = await fetch(gitcfUrl);
@@ -249,15 +280,17 @@ app.get('/gitcfcdn/:user/:repo/:branch/*', async (req, res) => {
         return res.sendStatus(404);
       }
 
-      const contentType = response.headers.get('content-type');
-      if (contentType && contentType.startsWith('image/')) {
-        res.setHeader('Content-Type', contentType);
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.startsWith("image/")) {
+        res.setHeader("Content-Type", contentType);
         response.body.pipe(res);
       } else {
         const file = await fetchFile(gitcfUrl);
         fileCache.set(cacheKey, file);
-        res.setHeader('Cache-Control', 'public, max-age=300');
-        res.writeHead(file.status, { 'Content-Type': file.contentType.split(';')[0] });
+        res.setHeader("Cache-Control", "public, max-age=300");
+        res.writeHead(file.status, {
+          "Content-Type": file.contentType.split(";")[0],
+        });
 
         res.locals.fileContent = file.content;
         await removeLeadingSlashFromAttributes(req, res, () => {
@@ -271,7 +304,7 @@ app.get('/gitcfcdn/:user/:repo/:branch/*', async (req, res) => {
   }
 });
 
-app.get('/staticallycdn/:user/:repo/:branch/*', async (req, res) => {
+app.get("/staticallycdn/:user/:repo/:branch/*", async (req, res) => {
   const { user, repo, branch } = req.params;
   const filePath = req.params[0];
 
@@ -281,8 +314,10 @@ app.get('/staticallycdn/:user/:repo/:branch/*', async (req, res) => {
     const cachedFile = fileCache.get(cacheKey);
 
     if (cachedFile) {
-      res.setHeader('Cache-Control', 'public, max-age=600');
-      res.writeHead(cachedFile.status, { 'Content-Type': cachedFile.contentType.split(';')[0] });
+      res.setHeader("Cache-Control", "public, max-age=600");
+      res.writeHead(cachedFile.status, {
+        "Content-Type": cachedFile.contentType.split(";")[0],
+      });
       res.end(cachedFile.content);
     } else {
       const response = await fetch(staticallyUrl);
@@ -291,15 +326,17 @@ app.get('/staticallycdn/:user/:repo/:branch/*', async (req, res) => {
         return res.sendStatus(404);
       }
 
-      const contentType = response.headers.get('content-type');
-      if (contentType && contentType.startsWith('image/')) {
-        res.setHeader('Content-Type', contentType);
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.startsWith("image/")) {
+        res.setHeader("Content-Type", contentType);
         response.body.pipe(res);
       } else {
         const file = await fetchFile(staticallyUrl);
         fileCache.set(cacheKey, file);
-        res.setHeader('Cache-Control', 'public, max-age=300');
-        res.writeHead(file.status, { 'Content-Type': file.contentType.split(';')[0] });
+        res.setHeader("Cache-Control", "public, max-age=300");
+        res.writeHead(file.status, {
+          "Content-Type": file.contentType.split(";")[0],
+        });
 
         res.locals.fileContent = file.content;
         await removeLeadingSlashFromAttributes(req, res, () => {
